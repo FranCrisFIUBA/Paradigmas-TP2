357665e21abe4de71fc1e60bd7997581f2c066ae
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:03:50 -0300

    Se agrego como parametro Ship ship a onShipEnter y onShipExit

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ITile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ITile.java
@@ -1,9 +1,11 @@
 package io.paradigmaticos.tiles;
 
+import io.paradigmaticos.ship.Ship;
+
 public interface ITile {
     ITileContent getContent();
     void setContent(ITileContent content);
     boolean isNavigable();
-    void onShipEnter();
-    void onShipExit();
+    void onShipEnter(Ship ship);
+    void onShipExit(Ship ship);
 }
