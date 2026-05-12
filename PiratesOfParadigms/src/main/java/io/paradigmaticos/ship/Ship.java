1b57f0ba46492bee97950dac826d03f5c4626149
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:09:31 -0300

    Se agregaron los atributos a Ship

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/ship/Ship.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/ship/Ship.java
@@ -3,6 +3,12 @@ package io.paradigmaticos.ship;
 import io.paradigmaticos.tiles.ITileContent;
 
 public class Ship implements ITileContent {
+    private final ShipAttribute healthAttribute = new ShipAttribute();
+    private final ShipAttribute attackAttribute = new ShipAttribute();
+    private final ShipAttribute defenseAttribute = new ShipAttribute();
+    private final ShipAttribute movesAttribute = new ShipAttribute();
+    private final ShipAttribute rangeAttribute = new ShipAttribute();
+
     @Override
     public String getName() {
         return "";
@@ -11,5 +17,25 @@ public class Ship implements ITileContent {
     @Override
     public boolean blocksNavigation() {
         return true;
+    }
+
+    public ShipAttribute getHealthAttribute() {
+        return healthAttribute;
+    }
+
+    public ShipAttribute getAttackAttribute() {
+        return attackAttribute;
+    }
+
+    public ShipAttribute getDefenseAttribute() {
+        return defenseAttribute;
+    }
+
+    public ShipAttribute getMovesAttribute() {
+        return movesAttribute;
+    }
+
+    public ShipAttribute getRangeAttribute() {
+        return rangeAttribute;
     }
 }
