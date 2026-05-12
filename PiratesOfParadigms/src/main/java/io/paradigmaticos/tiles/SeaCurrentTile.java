1c67713b98397873f0f128a756ccf8954781b106
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:39:29 -0300

    TODO: Implementar SeaCurrentTile

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/SeaCurrentTile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/SeaCurrentTile.java
@@ -2,6 +2,7 @@ package io.paradigmaticos.tiles;
 
 import io.paradigmaticos.ship.Ship;
 
+// TODO: Implementar SeaCurrentTile
 public class SeaCurrentTile extends AbstractTile {
     @Override
     public void onShipEnter(Ship ship) {
