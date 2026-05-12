7aa5bfbcb2656600a11025b4ff2ff61546164545
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:38:56 -0300

    TODO: Implementar LandTile

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/LandTile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/LandTile.java
@@ -2,6 +2,7 @@ package io.paradigmaticos.tiles;
 
 import io.paradigmaticos.ship.Ship;
 
+// TODO: Implementar LandTile
 public class LandTile extends AbstractTile {
     @Override
     public void onShipEnter(Ship ship) {
