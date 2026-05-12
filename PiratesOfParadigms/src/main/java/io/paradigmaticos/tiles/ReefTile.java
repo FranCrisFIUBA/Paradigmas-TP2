a6e00c0ffa5345177ee873f6f5bdc2416b6eee6d
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:39:22 -0300

    TODO: Implementar ReefTile

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ReefTile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ReefTile.java
@@ -2,6 +2,7 @@ package io.paradigmaticos.tiles;
 
 import io.paradigmaticos.ship.Ship;
 
+// TODO: Implementar ReefTile
 public class ReefTile extends AbstractTile {
     @Override
     public void onShipEnter(Ship ship) {
