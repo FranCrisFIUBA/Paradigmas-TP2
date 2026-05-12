c4d32f2da66a1b00eae12209851b458d72c6b789
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:05:34 -0300

    Se agregaron las clases concretas de AbstractTile sin implementar

---
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/MistTile.java
@@ -0,0 +1,15 @@
+package io.paradigmaticos.tiles;
+
+import io.paradigmaticos.ship.Ship;
+
+public class MistTile extends AbstractTile {
+    @Override
+    public void onShipEnter(Ship ship) {
+
+    }
+
+    @Override
+    public void onShipExit(Ship ship) {
+
+    }
+}
---
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/PortTile.java
@@ -0,0 +1,15 @@
+package io.paradigmaticos.tiles;
+
+import io.paradigmaticos.ship.Ship;
+
+public class PortTile extends AbstractTile {
+    @Override
+    public void onShipEnter(Ship ship) {
+
+    }
+
+    @Override
+    public void onShipExit(Ship ship) {
+
+    }
+}
