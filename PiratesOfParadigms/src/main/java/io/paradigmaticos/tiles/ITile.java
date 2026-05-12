e3240e1147b3317deec69f5ec06ecab8b54043c9
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 19:52:25 -0300

    Se removio ITile.setNavigable

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ITile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/ITile.java
@@ -4,7 +4,6 @@ public interface ITile {
     ITileContent getContent();
     void setContent(ITileContent content);
     boolean isNavigable();
-    boolean setNavigable(boolean navigable);
     void onShipEnter();
     void onShipExit();
 }
