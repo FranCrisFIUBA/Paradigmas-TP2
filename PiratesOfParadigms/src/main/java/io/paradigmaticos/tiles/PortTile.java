9d1283860374341593acfaeae84d32e65624fc6a
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:39:15 -0300

    TODO: Implementar PortTile

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/PortTile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/PortTile.java
@@ -2,6 +2,7 @@ package io.paradigmaticos.tiles;
 
 import io.paradigmaticos.ship.Ship;
 
+// TODO: Implementar PortTile
 public class PortTile extends AbstractTile {
     @Override
     public void onShipEnter(Ship ship) {
