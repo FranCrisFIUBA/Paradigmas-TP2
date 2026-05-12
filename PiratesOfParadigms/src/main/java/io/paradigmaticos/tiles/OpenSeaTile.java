125f2ac63a9f6b46a048332417d310a77706778e
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-12 20:39:09 -0300

    TODO: Implementar OpenSeaTile

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/OpenSeaTile.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/tiles/OpenSeaTile.java
@@ -2,6 +2,7 @@ package io.paradigmaticos.tiles;
 
 import io.paradigmaticos.ship.Ship;
 
+// TODO: Implementar OpenSeaTile
 public class OpenSeaTile extends AbstractTile {
     @Override
     public void onShipEnter(Ship ship) {
