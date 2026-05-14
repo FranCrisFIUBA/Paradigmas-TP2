ad91306434dd4f37e0e560229ca427fca67af696
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-14 13:21:54 -0300

    Se renombró BasicEffectHandler a EffectHandler

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/EffectHandler.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/EffectHandler.java
@@ -3,11 +3,11 @@ package io.paradigmaticos.effects;
 import java.util.LinkedList;
 import java.util.List;
 
-public class BasicEffectHandler<Effect extends IEffect<Target>, Target> implements IEffectHandler<Effect, Target> {
+public class EffectHandler<Effect extends IEffect<Target>, Target> implements IEffectHandler<Effect, Target> {
     private final Target target;
     private final List<Effect> effects = new LinkedList<>();
 
-    public BasicEffectHandler(Target target) {
+    public EffectHandler(Target target) {
         if (target == null) {
             throw new IllegalArgumentException("Target object cannot be null");
         }
