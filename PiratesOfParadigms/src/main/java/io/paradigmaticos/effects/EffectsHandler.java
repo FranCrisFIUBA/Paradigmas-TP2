86e2602d51b0f5e5e86baa9fd0e5495aa52084f6
Franco Cristaldo <francocristaldo@gmail.com>
2026-05-14 13:38:59 -0300

    renombrado EffectHandler a EffectsHandler

--- PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/EffectsHandler.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/EffectsHandler.java
@@ -3,11 +3,11 @@ package io.paradigmaticos.effects;
 import java.util.LinkedList;
 import java.util.List;
 
-public class EffectHandler<Effect extends IEffect<Target>, Target> implements IEffectHandler<Effect, Target> {
+public class EffectsHandler<Effect extends IEffect<Target>, Target> implements IEffectsHandler<Effect, Target> {
     private final Target target;
     private final List<Effect> effects = new LinkedList<>();
 
-    public EffectHandler(Target target) {
+    public EffectsHandler(Target target) {
         if (target == null) {
             throw new IllegalArgumentException("Target object cannot be null");
         }
--- PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/IEffectsHandler.java
+++ PiratesOfParadigms/src/main/java/io/paradigmaticos/effects/IEffectsHandler.java
@@ -2,7 +2,7 @@ package io.paradigmaticos.effects;
 
 import java.util.List;
 
-public interface IEffectHandler<Effect extends IEffect<Target>, Target> {
+public interface IEffectsHandler<Effect extends IEffect<Target>, Target> {
     Target getTarget();
     List<Effect> getEffects();
     void clearEffects();
